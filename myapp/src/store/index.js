4ec11527d754cb994e03f173e40fba80ec2c1226
Sowmya Billakanti <sowmya.billakanti8@gmail.com>
2021-01-27 09:40:16 -0800

    Added Cart page

--- myapp/src/store/index.js
+++ myapp/src/store/index.js
@@ -3,11 +3,11 @@ import { createStore, combineReducers } from 'redux';
 // This dependency enables the Redux Dev Tools in your Chrome Console.
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-
+import cart from './cartReducer';
 import products from './products-reducer';
 
 // combine our reducers - right now it isn't necessary, however, you usually will have more than one and it will be.
-let reducers = combineReducers({ products });
+let reducers = combineReducers({ products, cart });
 
 // finally, we get to actually create the store
 const store = () => {
